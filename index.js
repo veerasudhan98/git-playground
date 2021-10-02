de64d4a02aa676ceb3c1978d0f1184f520bbd70f
Veera Sudhan <60940029+veerasudhan98@users.noreply.github.com>
2021-10-02 18:15:44 +0530

    [Closes #2] changed subtraction function to new branch (#10)

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ console.log("hello");
 
 console.log("changed by dev 2");
 
-function addition() {
-  console.log(10 + 20);
-}
 function subtraction() {
   console.log(20 - 10);
 }
 
-addition();
+subtraction();
